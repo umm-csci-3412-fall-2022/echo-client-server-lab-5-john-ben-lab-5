7b9cca65e217b0b8d336733297dec0454b72c292
jpwalbran <walbr042@morris.umn.edu>
2022-10-20 16:17:37 -0500

    Implement client
    Implements a client for the echo server, which currently compiles and runs,
    but does not pass the tests.
    Co-authored-by: BananaObserved <golds349@morris.umn.edu>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,49 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoClient {
+
+  public static final int PORT_NUMBER = 6013;
+
+  public static void main(String[] args) {
+
+    String hostname;
+
+    if (args.length == 0) {
+      hostname = "127.0.0.1";
+    } else {
+      hostname = args[0];
+    }
+
+    //Create a connection between the client and the specified server
+    try {
+      Socket socket = new Socket(hostname, PORT_NUMBER);
+
+
+      InputStream inStream = socket.getInputStream();
+      OutputStream outStream = socket.getOutputStream();
+
+      int nextByte;
+      //Continually process bytes from the input stream until EOF
+      //EOF is represented as -1
+      while ((nextByte = System.in.read()) != -1) {
+        outStream.write(nextByte);
+        System.out.write(inStream.read());
+      }
+
+      //Final cleanup
+      inStream.close();
+      outStream.flush();
+      socket.shutdownOutput();
+
+    } catch (ConnectException ce) {
+      System.out.println("We were unable to connect to server " + hostname);
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception!");
+      System.out.println(ioe);
+    }
+  }
+
 }
