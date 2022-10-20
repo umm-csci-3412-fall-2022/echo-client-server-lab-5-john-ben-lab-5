088af8ec705cae5051fcc54f4f73ff2c87458f23
jpwalbran <walbr042@morris.umn.edu>
2022-10-20 15:51:28 -0500

    Implement the server
    Created an implementation of the server that passes the tests using the
    InputStream.transferTo() method.
    
    Note that this implementation requires Java 9+.
    
    Co-authored-by: BanaanObserved <golds349@morris.umn.edu>

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,41 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+
+  public static final int PORT_NUMBER = 6013;
+
+  public static void main(String[] args) {
+    try {
+
+      ServerSocket sock = new ServerSocket(PORT_NUMBER);
+
+      //Wait for a client to create a connection
+      while (true) {
+        Socket client = sock.accept();
+
+        InputStream inStream = client.getInputStream();
+        OutputStream outStream = client.getOutputStream();
+
+        //Reads from the input stream to the output,
+        //stopping when the stream finishes
+        inStream.transferTo(outStream);
+
+        //Finish up by closing the connections
+        inStream.close();
+        outStream.flush(); //Forcibly empties the internal buffer
+        outStream.close();
+        client.close();
+
+      }
+
+    } catch(IOException ioe) {
+      System.out.println("Caught the following exception: ");
+      System.out.println(ioe);
+    }
+
+    System.out.println("Server Works!");
+  }
 }
